fae81da2bbba43f0a9acdb5ae4cafcd6a4e5f553
kathleen <catlean4@gmail.com>
2023-09-05 17:26:31 -0600

    add turret shop + fixed currency bug
    fixed issue where gold wouldn't subtract when a turret was purchased

---
+++ tower defense/Assets/Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    //link currency to UI
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI currencyUI;
+
+    private void OnGUI() {
+        currencyUI.text = LevelManager.main.gold.ToString();
+    }
+}
