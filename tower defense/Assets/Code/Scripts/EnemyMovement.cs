5c5873eade5f512dbeac79c4bb180b4669c84f0d
kathleen <catlean4@gmail.com>
2023-09-05 20:20:10 -0600

    add Ice Turret
    slows enemies but doesn't do damage

--- tower defense/Assets/Code/Scripts/EnemyMovement.cs
+++ tower defense/Assets/Code/Scripts/EnemyMovement.cs
@@ -13,7 +13,10 @@ public class EnemyMovement : MonoBehaviour
     private Transform target;
     private int pathIndex = 0;
 
+    private float baseSpeed;
+
     private void Start() {
+        baseSpeed = moveSpeed; //set up ice tower
         target = LevelManager.main.path[pathIndex];
     }
 
@@ -38,5 +41,13 @@ public class EnemyMovement : MonoBehaviour
         Vector2 direction = (target.position - transform.position).normalized;
 
         rb.velocity = direction * moveSpeed;
+    }
+
+    public void UpdateSpeed(float newSpeed) {
+        moveSpeed = newSpeed;
+    }
+
+    public void ResetSpeed() {
+        moveSpeed = baseSpeed;
     }
 }
