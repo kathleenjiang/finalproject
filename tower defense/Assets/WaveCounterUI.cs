d5c1cffbb386787e63e9e2fe25896a1dbc9f6ab4
kathleen <catlean4@gmail.com>
2023-09-05 21:26:44 -0600

    add wave counter UI

---
+++ tower defense/Assets/WaveCounterUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+
+public class WaveCounterUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI waveUI;
+    [SerializeField] EnemySpawner enemySpawner; // Reference to the specific EnemySpawner instance
+
+    private void OnGUI() {
+        waveUI.text = "Wave: " + enemySpawner.currentWave.ToString() + "/20";
+    }
+
+    public void SetSelected() {
+    }
+}
