f30885c8e42129f93e4612b2f99106938afc8023
kathleen <catlean4@gmail.com>
2023-09-07 14:54:09 -0600

    add visible range to turrets

---
+++ tower defense/Assets/Code/Scripts/MouseOverRange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverRange : MonoBehaviour
+{
+    private SpriteRenderer targetingRangeCircle;
+
+    private void Start() {
+        targetingRangeCircle = GetComponentInChildren<SpriteRenderer>();
+        targetingRangeCircle.enabled = false; //hide targeting range
+    }
+
+    private void OnMouseEnter() {
+        // on mouse over
+        targetingRangeCircle.enabled = true;
+    }
+
+    private void OnMouseExit() {
+        targetingRangeCircle.enabled = false;
+    }
+}
+
+
+
+
+
