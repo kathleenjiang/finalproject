ae79d5951c2bf298237f68167125b9deb36c81a1
kathleen <catlean4@gmail.com>
2023-09-07 16:16:21 -0600

    Fix bullet speed and targeting
    adjusted bullet.cs

--- tower defense/Assets/Code/Scripts/Bullet.cs
+++ tower defense/Assets/Code/Scripts/Bullet.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour 
+{
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
 
     [Header("Attributes")]
-    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private float bulletSpeed = 10f;
     [SerializeField] private int bulletDamage = 1;
 
 
@@ -20,9 +21,16 @@ public class Bullet : MonoBehaviour {
 
     private void FixedUpdate() {
         if (!target) return;
+
+        Vector3 targetPosition = target.position + (Vector3)(target.GetComponent<Rigidbody2D>().velocity * Time.fixedDeltaTime);
+
         Vector2 direction = (target.position - transform.position).normalized;
 
         rb.velocity = direction * bulletSpeed;
+
+        // rotate vertical projectile to face the target
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f; 
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
     }
 
